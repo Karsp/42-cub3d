c67623af6830eb8d82f6797d2e7567d4da1b5ae3
Daniel Francisco Lanzas Pellico <dlanzas-@c1r14s4.42madrid.com>
2024-08-07 17:39:30 +0200

    Cleaning draw.c

--- src/draw.c
+++ src/draw.c
@@ -6,7 +6,7 @@
 /*   By: dlanzas- <dlanzas-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 13:47:15 by dlanzas-          #+#    #+#             */
-/*   Updated: 2024/08/07 14:03:04 by dlanzas-         ###   ########.fr       */
+/*   Updated: 2024/08/07 14:14:40 by dlanzas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,25 @@ void	draw_map(t_game *game)
 	long	count;
 
 	map = game->map;
-	aux = 0;
+	aux = -1;
 	count = -1;
 	draw_square((t_square){0, 0, GRIDSIZE, game->map->c_color}, *game->img);
 	draw_square((t_square){0, 0, GRIDSIZE, game->map->f_color}, *game->img2);
 	while (game->map->checked_map[++count])
 	{
-		while (aux < game->map->num_cols)
+		while (++aux < game->map->num_cols)
 		{
-			if (game->map->checked_map[count][aux] == '1')
+			if ((map->checked_map[count][aux]) == '1')
+			{
 				if (mlx_image_to_window(game->mlx, game->img, aux * GRIDSIZE, \
 				count * GRIDSIZE) < 0)
 					c_error("Image to window error\n");
-			else if (game->map->checked_map[count][aux] == '0')
+			}
+			else if (map->checked_map[count][aux] == '0')
 				if (mlx_image_to_window(game->mlx, game->img2, aux * GRIDSIZE, \
 				count * GRIDSIZE) < 0)
 					c_error("Image to window error\n");
-			aux++;
 		}
-		aux = 0;
+		aux = -1;
 	}
 }
